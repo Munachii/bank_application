b5b39e2ada72da87a6b8c73c0af4614c1fdb85b4
Sonny <local.user@LOAN-17288.bhasvic.internal>
2022-07-25 16:09:47 +0100

    created some method in the account class

--- src/main/java/bank_application/account.java
+++ src/main/java/bank_application/account.java
@@ -8,17 +8,36 @@ public class account {
     private int customerID;
 
 
-public account( String customerNameP, int CustomerIDP) {
-    this.customerName = customerNameP;
-    this.customerID = CustomerIDP;
-}    
-    //int balanceP, int previousTransactionP, String customerNameP,
-// //if (balanceP<=0 || customerNameP == null){
-//     getError();
-// }
-    public String getError(){
-        String error = "This is an invalid entry please try again";
-        return(error);
+    public account( String cName, int cID) {
+        this.customerName = cName;
+        this.customerID = cID;
+    } 
+    void diposit (int amount ){
+        if(amount >=0){
+            balance = balance + amount;
+            previousTransaction =amount;
+        }
     }
+    void withdraw (int amount ){
+        if(amount >=0){
+            balance = balance - amount;
+            previousTransaction = amount;
+        }
+    }
+
+    void getPreviousTransaction(){
+        if (previousTransaction >0){
+            System.out.println("Deposited: "+previousTransaction );
+        }
+        else if (previousTransaction <0){{
+            System.out.println("withdrawn: "+previousTransaction );
+
+        }}
+    }
+        //int balanceP, int previousTransactionP, String customerNameP,
+    // //if (balanceP<=0 || customerNameP == null){
+    //     getError();
+    // }
+    
 }
 //https://www.youtube.com/watch?v=hqY6rKhRBzU 5:19
--- src/main/java/bank_application/main.java
+++ src/main/java/bank_application/main.java
@@ -1,19 +1,30 @@
 package bank_application;
 import java.util.Scanner;
 public class main {
-    public static void main (String[] args){
+    int i =0;
+    int array[];
+    public static String getError(){
+        String error = "This is an invalid entry please try again";
+        return(error);
+    } 
+    public void main (String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Would you like to open up an account today (1) or check a current account? (2) ")
         String answer = scanner.nextLine();
         if (answer == "1"){
+            System.out.println("enter name");
+            String name = scanner.nextLine();
 
-        }
-        else if( answer == "1"){
+            }
+        
+        else if( answer == "2"){
 
         }
         else {
             getError();
         }
 
+    }  
     }
-}
+       
+
