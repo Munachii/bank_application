e908774117fb600c6092ac99d0119cbc7bbe75c2
Sonny <local.user@LOAN-17288.bhasvic.internal>
2022-07-26 15:18:46 +0100

    added some methods

--- src/main/java/bank_application/account.java
+++ src/main/java/bank_application/account.java
@@ -21,7 +21,7 @@ public class account {
     void withdraw (int amount ){
         if(amount >=0){
             balance = balance - amount;
-            previousTransaction = amount;
+            previousTransaction = -amount;
         }
     }
 
@@ -29,11 +29,30 @@ public class account {
         if (previousTransaction >0){
             System.out.println("Deposited: "+previousTransaction );
         }
-        else if (previousTransaction <0){{
-            System.out.println("withdrawn: "+previousTransaction );
-
-        }}
-    }
+        else if (previousTransaction <0){
+            System.out.println("withdrawn: "+Math.abs(previousTransaction));
+        
+        }
+        else {
+            System.out.println("No transcation has occured");
+        }
+        }
+        void calculateInterestFixed(int years){
+            double interestRate = .0185;
+            double interest = (balance*interestRate*years )
+        }
+        
+        void calculateInterestComp(int years){
+            double interestRate = .0185;
+            int i=0;
+            double newBalance = balance;
+            while (i < years){
+                newBalance= newBalance*1.0185;
+                i++; 
+            }
+            
+            double interest = (newBalance - balance);
+        }
         //int balanceP, int previousTransactionP, String customerNameP,
     // //if (balanceP<=0 || customerNameP == null){
     //     getError();
