40d0da59fe1725d8023a990f02e0901c3d12f6cd
Sonny <local.user@LOAN-17288.bhasvic.internal>
2022-07-30 13:36:25 +0100

    added menu

--- src/main/java/bank_application/account.java
+++ src/main/java/bank_application/account.java
@@ -1,4 +1,5 @@
 package bank_application;
+
 import java.util.Scanner;
 
 public class account {
@@ -7,56 +8,77 @@ public class account {
     private String customerName;
     private int customerID;
 
-
-    public account( String cName, int cID) {
+    public account(String cName, int cID) {
         this.customerName = cName;
         this.customerID = cID;
-    } 
-    void diposit (int amount ){
-        if(amount >=0){
+    }
+
+    void diposit(int amount) {
+        if (amount >= 0) {
             balance = balance + amount;
-            previousTransaction =amount;
+            previousTransaction = amount;
         }
     }
-    void withdraw (int amount ){
-        if(amount >=0){
+
+    void withdraw(int amount) {
+        if (amount >= 0) {
             balance = balance - amount;
             previousTransaction = -amount;
         }
     }
 
-    void getPreviousTransaction(){
-        if (previousTransaction >0){
-            System.out.println("Deposited: "+previousTransaction );
-        }
-        else if (previousTransaction <0){
-            System.out.println("withdrawn: "+Math.abs(previousTransaction));
-        
-        }
-        else {
+    void getPreviousTransaction() {
+        if (previousTransaction > 0) {
+            System.out.println("Deposited: " + previousTransaction);
+        } else if (previousTransaction < 0) {
+            System.out.println("withdrawn: " + Math.abs(previousTransaction));
+
+        } else {
             System.out.println("No transcation has occured");
         }
-        }
-        void calculateInterestFixed(int years){
+    }
+
+    void calculateInterestFixed(int years){
             double interestRate = .0185;
             double interest = (balance*interestRate*years )
+            double newBalance =(balance+interest);
+            System.out.println("At 1.85% fixed interest you have gained "+interest+"over"+years+"years");
+            System.out.println("Your new balance is" +newBalance);
         }
-        
-        void calculateInterestComp(int years){
-            double interestRate = .0185;
-            int i=0;
-            double newBalance = balance;
-            while (i < years){
-                newBalance= newBalance*1.0185;
-                i++; 
-            }
-            
-            double interest = (newBalance - balance);
+
+    void calculateInterestComp(int years) {
+        double interestRate = .0185;
+        int i = 0;
+        double interest = 0;
+        while (i < years) {
+            interest = interest * interestRate;
+            i++;
         }
-        //int balanceP, int previousTransactionP, String customerNameP,
+        double newBalance = (balance + interest);
+        System.out.println("At 1.85% compound interest you have gained " + interest + "over" + years + "years");
+        System.out.println("Your new balance is" + newBalance);
+    }
+
+    void showMenu() {
+        char option = "\0";
+        Scanner scanner = new Scanner(System.in);
+        Scanner(System.in);
+        System.out.println("Welcome," + customerName + "I");
+        System.out.println("Your ID is: " + customerID);
+        System.out.println();
+        System.out.println("What would you like to do?");
+        System.out.println();
+        System.out.println("A. Check your balance");
+        System.out.println("B. Make a deposit");
+        System.out.println("C. Make a withdrawal");
+        System.out.println("D. View previous transaction");
+        System.out.println("E. Calculate interest");
+        System.out.println("F. Exit");
+    }
+    // int balanceP, int previousTransactionP, String customerNameP,
     // //if (balanceP<=0 || customerNameP == null){
-    //     getError();
+    // getError();
     // }
-    
+
 }
-//https://www.youtube.com/watch?v=hqY6rKhRBzU 5:19
+// https://www.youtube.com/watch?v=hqY6rKhRBzU 5:19
